18d0a85a44af9279f9b5125d22e74f225bc7f1ec
Joe Bush <arcandio@gmail.com>
2025-01-10 08:59:49 -0600

    finished #60

--- AVDR/Assets/Code/IO/Editor/EditorPlayerPrefs.cs
+++ AVDR/Assets/Code/IO/Editor/EditorPlayerPrefs.cs
@@ -3,20 +3,20 @@ using UnityEditor;
 
 public class EditorPlayerPrefs : MonoBehaviour
 {
-    [MenuItem("AVDR/Clear Player Prefs")]
+    [MenuItem("AVDR/Clear Player Prefs", priority = 1)]
     static void ClearPlayerPrefs() {
         Debug.LogWarning("Clearing PlayerPrefs");
         PlayerPrefs.DeleteAll();
     }
 
-    [MenuItem("AVDR/Dump character data to Clipboard")]
+    [MenuItem("AVDR/Dump character data to Clipboard", priority = 2)]
     static void DumpCharacters() {
         string temp = PlayerPrefs.GetString(IoSystem.characterDataKey);
         EditorGUIUtility.systemCopyBuffer = temp;
         Debug.Log("character data copied to clipboard.");
     }
 
-    [MenuItem("AVDR/Dump history data to Clipboard")]
+    [MenuItem("AVDR/Dump history data to Clipboard", priority = 3)]
     static void DumpHistory() {
         string temp = PlayerPrefs.GetString(IoSystem.historyKey);
         EditorGUIUtility.systemCopyBuffer = temp;
