837f4b61b6dafe2fc849ebd26fbb20f0991bcc32
Joe Bush <arcandio@gmail.com>
2025-01-15 16:14:17 -0600

    working on #24, #76, #66

---
+++ AVDR/Assets/Code/Ui/LightingTilt.cs
@@ -0,0 +1,20 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+public class LightingTilt : MonoBehaviour
+{
+    public float speed = 50;
+    void Start() {
+        Input.gyro.enabled = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 target = Input.gyro.attitude.eulerAngles;
+        target = new Vector3(target.y * 1, (target.z -90f) * 1, target.x * -1);
+        Quaternion targetQuat = Quaternion.Euler(target);
+        // transform.eulerAngles = target;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetQuat, speed * Time.deltaTime);
+        Debug.Log(target);
+    }
+}
